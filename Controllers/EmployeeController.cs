5de2dd03b4e265f0c4bd2de38d987eb7c28f6192
Nelisa Mandla <nelisajola@gmail.com>
2025-07-14 23:51:04 +0200

    Basic Employee CRUD

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Cafeteria_Credit___Ordering_System.Data;
+
+namespace Cafeteria_Credit___Ordering_System.Controllers
+{
+    public class EmployeeController: Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeeController(ApplicationDbContext context)
+    {
+        _context = context;
+        }
+        // GET: list of employees
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Employees.ToListAsync());
+        }
+        // GET: Create a new employee
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create()
+        {
+            if (ModelState.IsValid)
+            {
+                var employee = new Models.Employee
+                {
+                    // Initialize properties here
+                };
+                _context.Employees.Add(employee);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+        //GET: Get employee by id
+        public async Task<IActionResult> Details(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+    }
+}
